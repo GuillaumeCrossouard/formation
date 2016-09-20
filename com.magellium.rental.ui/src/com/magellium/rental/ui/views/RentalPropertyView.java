e7407dfe1bd32f89c620007bf47283a66c2cae90
gcd <gcd@PC245.magellium.com>
2016-09-20 15:14:55 +0200

    drag & drop

--- com.magellium.rental.ui/src/com/magellium/rental/ui/views/RentalPropertyView.java
+++ com.magellium.rental.ui/src/com/magellium/rental/ui/views/RentalPropertyView.java
@@ -5,6 +5,13 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DragSource;
+import org.eclipse.swt.dnd.DragSourceAdapter;
+import org.eclipse.swt.dnd.DragSourceEvent;
+import org.eclipse.swt.dnd.DragSourceListener;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -86,6 +93,7 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener{
 		gd.horizontalSpan = 3; // occupe les 2 premières colonnes
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
+		setLabelAsDragSource(rentedObjectLabel);
 		
 		lblLou = new Label(infoGroup, SWT.NONE);
 		lblLou.setText("Lou\u00E9 \u00E0 :");
@@ -95,6 +103,7 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener{
 		gd2.horizontalSpan = 2;
 		gd2.horizontalAlignment = SWT.FILL;
 		customerLabel.setLayoutData(gd2);
+		setLabelAsDragSource(customerLabel);
 		
 		Group dateGroup = new Group(parent, SWT.NONE);
 		GridData gd_dateGroup = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
@@ -200,6 +209,23 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener{
 			}
 		}
 	}
+	
+	//drag & drop
+	public void setLabelAsDragSource(final Label label){
+		DragSource source = new DragSource(label, DND.DROP_MOVE | DND.DROP_COPY);
+		
+		source.setTransfer(new Transfer[]{TextTransfer.getInstance()});
+		
+		source.addDragListener(new DragSourceAdapter(){
+			@Override
+			public void dragSetData(DragSourceEvent event) {
+				if (TextTransfer.getInstance().isSupportedType(event.dataType)){
+					event.data = label.getText();
+				}
+			}
+		});
+	}
+	
 
 	
 }
