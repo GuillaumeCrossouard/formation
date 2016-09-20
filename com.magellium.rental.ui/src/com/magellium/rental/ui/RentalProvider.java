d08c0f13065097feda468774a0b77471b2f4e77d
gcd <gcd@PC245.magellium.com>
2016-09-20 13:55:40 +0200

    Ajout des icones

--- com.magellium.rental.ui/src/com/magellium/rental/ui/RentalProvider.java
+++ com.magellium.rental.ui/src/com/magellium/rental/ui/RentalProvider.java
@@ -2,15 +2,21 @@ package com.magellium.rental.ui;
 
 import java.util.Collection;
 
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class RentalProvider extends LabelProvider implements ITreeContentProvider {
+public class RentalProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
 
 	private static final Object[] EMPTY_RESULT = new Object[0];
 
@@ -68,6 +74,51 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 			return ((Customer) element).getFirstName() + " " + ((Customer) element).getLastName();
 		}
 		return super.getText(element);
+	}
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof RentalAgency) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_BLUE);
+		}
+		if (element instanceof Rental) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
+		}
+		if (element instanceof RentalObject) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+		}
+		if (element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_MAGENTA);
+		}
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		if (element instanceof Node) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW);
+		}
+		return null;
+	}
+	
+	@Override
+	public Image getImage(Object element) {
+		
+		ImageRegistry imageRegistry = RentalUIActivator.getDefault().getImageRegistry();
+		
+		if (element instanceof Customer) {
+			return imageRegistry.get(RentalUIActivator.IMG_CUSTOMER);
+		}
+		if (element instanceof RentalAgency) {
+			return imageRegistry.get(RentalUIActivator.IMG_AGENCY);
+		}
+		if (element instanceof Rental) {
+			return imageRegistry.get(RentalUIActivator.IMG_RENTAL);
+		}
+		if (element instanceof RentalObject) {
+			return imageRegistry.get(RentalUIActivator.IMG_OBJECT);
+		}
+		return super.getImage(element);
 	}
 
 }
--- com.magellium.rental.ui/src/com/magellium/rental/ui/RentalUIActivator.java
+++ com.magellium.rental.ui/src/com/magellium/rental/ui/RentalUIActivator.java
@@ -1,7 +1,13 @@
 package com.magellium.rental.ui;
 
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.launch.Framework;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -10,6 +16,11 @@ public class RentalUIActivator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.magellium.rental.ui"; //$NON-NLS-1$
+
+	public static final String IMG_CUSTOMER = "icons/Customers.png";
+	public static final String IMG_RENTAL = "icons/Rentals.png";
+	public static final String IMG_OBJECT = "icons/RentalObjects.png";
+	public static final String IMG_AGENCY = "icons/Agency.png";
 
 	// The shared instance
 	private static RentalUIActivator plugin;
@@ -45,6 +56,16 @@ public class RentalUIActivator extends AbstractUIPlugin {
 	 */
 	public static RentalUIActivator getDefault() {
 		return plugin;
+	}
+	
+	@Override
+	protected void initializeImageRegistry(ImageRegistry reg) {
+		Bundle b = FrameworkUtil.getBundle(this.getClass());
+		
+		reg.put(IMG_CUSTOMER,  ImageDescriptor.createFromURL(b.getEntry(IMG_CUSTOMER)));
+		reg.put(IMG_RENTAL,  ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL)));
+		reg.put(IMG_OBJECT,  ImageDescriptor.createFromURL(b.getEntry(IMG_OBJECT)));
+		reg.put(IMG_AGENCY,  ImageDescriptor.createFromURL(b.getEntry(IMG_AGENCY)));
 	}
 
 }
